4499da9d1903e59fb8ea6ceaa795ed04535478e6
Emma Finkel <emma.finkel@gmail.com>
2023-04-05 11:22:18 -0400

    Initial Commit

---
+++ DatePicker/DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerApp.swift
+//  DatePicker
+//
+//  Created by scholar on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
